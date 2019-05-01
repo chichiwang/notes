948039b1c38c8d4c13b2353df449d684df17e8e7
Chi-chi Wang <chichi@wangchiyi.com>
2019-04-30 21:07:49 -0500

    [get_rates] Pretty print the JSON response

--- get_rates.py
+++ get_rates.py
@@ -1,4 +1,7 @@
 import requests
+import json
 
 response = requests.get("https://api.exchangeratesapi.io/latest?symbols=USD")
-print(response.text)
+usd_rates = json.loads(response.text)
+
+print(json.dumps(usd_rates, sort_keys=True, indent=2))
